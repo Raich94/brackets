c7c3d8917333112ddb74de6ce0c38a3a0b59a037
Raich94 <raichxxx@mail.ru>
2023-02-12 20:50:43 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const bracketsPairs = {};
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    bracketsPairs[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  console.log(openBrackets)
+  console.log(bracketsPairs)
+  
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++){
+    if (openBrackets.includes(str[i])){
+      if (stack.includes(str[i])&&str[i] == '|' || stack.includes(str[i])&&str[i] == '7' || stack.includes(str[i])&&str[i] == '8'){
+        stack.pop()
+      }else {
+        stack.push(str[i])
+      }
+    } else {
+      if (stack.length === 0){
+        return false
+      }
+      let topStack = stack[stack.length - 1];
+       if (bracketsPairs[str[i]] == topStack){
+         stack.pop();
+       }else {
+         return false
+       }
+    }
+  }
+  return stack.length === 0
 }
